6ac0c0f2f5d557373a43a5e9c0e6d1e39173fca5
adam.walid <adam.0522018@gmaqil.com>
2025-05-05 12:05:31 +0300

    Add project files.

---
+++ Lagrange Interpolation.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+double lagrangeInterpolation(double x, vector<double>X, vector<double> Y) {
+    int n = X.size();
+
+    for (int i = 0;i < n;++i) {
+        if (X[i] > x)
+        {
+            double x0 = X[i - 1], x1 = X[i];
+            double y0 = Y[i - 1], y1 = Y[i];
+
+            double term1 = y0 * ((x - x1) / (x0 - x1));
+            double term2 = y1 * ((x - x0) / (x1 - x0));
+            return term1 + term2;
+        }
+        cout << "invalid";
+    }
+}
+
+int main()
+{
+    vector<double> x = { 1,2,2.5,3,4,5 };
+    vector<double> y = { 1,5,7,8,2,1 };
+    double value = 3.4;
+
+    double res = lagrangeInterpolation(value, x, y);
+    cout << "The interporid value at x = " << value << "is " << res;
+    //printf("%.6f\n" ,res);
+    return 0;
+}
